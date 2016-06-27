27e19f019f3294e5c69c76ce5f930a2e401ae72f
Jesse Nayak <jdn4ae@virginia.edu>
2016-06-27 15:58:14 -0400

    Further simplifies submission list endpoint

--- meetings/api/serializers.py
+++ meetings/api/serializers.py
@@ -19,17 +19,17 @@ class TagSerializer(serializers.ModelSerializer):
 		model = Tag
 		fields = ('id', 'lower',)
 
+class ConferenceSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Conference
+		fields = ('created', 'modified', 'id', 'title', 'website', 'city', 
+			'state', 'country', 'start_date', 'end_date', 'submission_date', 'close_date', 'logo_url', 'tags', 'sponsors', 'description')
+
 class SubmissionSerializer(serializers.ModelSerializer):
-	conference = serializers.HyperlinkedRelatedField(many=False, read_only=True, view_name='conference-detail')
+	conference = ConferenceSerializer()
 	tags = TagSerializer(many=True)
 	contributors = serializers.PrimaryKeyRelatedField(many=False, queryset=Group.objects.all())
 	node_id = serializers.CharField(read_only=True)
 	class Meta:
 		model = Submission
 		fields = ('id', 'node_id', 'title', 'description', 'conference', 'tags', 'contributors')
-
-class ConferenceSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Conference
-		fields = ('created', 'modified', 'id', 'title', 'website', 'city', 
-			'state', 'country', 'start_date', 'end_date', 'submission_date', 'close_date', 'logo_url', 'tags', 'sponsors', 'description')
