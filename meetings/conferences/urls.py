579fa16e21b580f2469e3c098a26ec480ea0dba5
Jesse Nayak <jdn4ae@virginia.edu>
2016-07-07 14:55:04 -0400

    Change url config for slugs

--- meetings/conferences/urls.py
+++ meetings/conferences/urls.py
@@ -3,7 +3,7 @@ from conferences import views
 
 urlpatterns = [
     url(r'^$', views.ConferenceList.as_view(), name='list'),
-    url(r'^(?P<pk>[0-9]+)/$', views.ConferenceDetail.as_view(), name='detail'),
-    url(r'^(?P<conference_id>[0-9]+)/submissions/',
+    url(r'^(?P<pk>[-\w]+)/$', views.ConferenceDetail.as_view(), name='detail'),
+    url(r'^(?P<conference_id>[-\w]+)/submissions/',
         include('submissions.urls', namespace='submissions')),
 ]
