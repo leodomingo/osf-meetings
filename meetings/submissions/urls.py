df2ae07a5503dcad8f6bb972e454e60ea6200980
Bryan Gorges <bgorges@cos.io>
2016-07-07 14:30:22 -0400

    Converted conference to json api

--- meetings/submissions/urls.py
+++ meetings/submissions/urls.py
@@ -3,5 +3,5 @@ from submissions import views
 
 urlpatterns = [
     url(r'^$', views.SubmissionList.as_view(), name='list'),
-    url(r'^(?P<pk>[0-9]+)/$', views.SubmissionDetail.as_view(), name='detail'),
+    url(r'^(?P<submission_id>[0-9]+)/$', views.SubmissionDetail.as_view(), name='detail'),
 ]
