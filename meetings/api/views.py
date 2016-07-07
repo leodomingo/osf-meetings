998a8401974cb8675d6447586c79ea3dfd80e8a2
TomHeatwole <tgh8xn@virginia.edu>
2016-07-07 10:59:03 -0400

    Removing console logs and adding to submission page. (Ready for Code Review now)

--- meetings/api/views.py
+++ meetings/api/views.py
@@ -10,14 +10,8 @@ from rest_framework.response import Response
 from django.contrib.auth import authenticate, login
 from rest_framework import status
 
-# from django.utils.decorators import method_decorator
-# from django.contrib.auth.decorators import login_required
-
 class checkLoggedIn(APIView):
-    #@method_decorator(login_required)
     def get(self, request, format=None):
-        print(request.user)
-        print(request.session.session_key)
         if request.user.is_authenticated():
             return Response('true')
         else:
