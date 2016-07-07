4c14375c650d788f2cbcbe70bb6b7da675184fce
Tav0 <lv23@vt.edu>
2016-07-06 21:34:35 -0400

    fix embercli's message errors

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	$_GET: function(name){
+	$_GET: function(name) {
 		  var url = window.location.search;
 		  var num = url.search(name);
 		  var namel = name.length;
@@ -10,8 +10,8 @@ export default Ember.Route.extend({
 		  url = url.replace(front, "");  
 		  num = url.search("&");
 
-		 if(num>=0) return url.substr(0,num); 
-		 if(num<0)  return url;   
+		 if(num>=0) { return url.substr(0,num); }
+		 if(num<0) { return url; }
 	},
 
 	activate: function() {
@@ -19,7 +19,8 @@ export default Ember.Route.extend({
 		//backend to check if the user is logged in, or if they need to go to login page
 		var code = this.$_GET('code');
 		var self = this;
-		if (code == '') {
+
+		if (code === '') {
 			window.location="https://staging-accounts.osf.io/oauth2/authorize?scope=osf.full_read+osf.full_write&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Flogin&response_type=code&client_id=d5c46638ed1d42b9977264d084875c5a";
 		} else {
 			Ember.$.ajax({
