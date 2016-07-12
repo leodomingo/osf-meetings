48dacb0c1553f00172f868f01384fc9338af0c6c
Leandro Alberto-Dominguez <la5aj@virginia.edu>
2016-07-12 12:52:37 -0400

    adding option for preloading test conferences

--- meetings/script.sh
+++ meetings/script.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 
+conf=$1
+
 rm -rf db.sqlite
 rm api/migrations/0*
 ./manage.py makemigrations
 ./manage.py migrate
-./manage.py loadtestdata conferences.Conference:10000
+if [[ -n "$conf" ]]; then
+	./manage.py loadtestdata conferences.Conference:${conf}
+else 
+	echo "creating empty database"
+fi
 ./manage.py runserver
