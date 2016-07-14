e2a1e74a7be11df404d5e3965cb38b8911174f0b
Leandro Alberto-Dominguez <la5aj@virginia.edu>
2016-07-14 11:47:20 -0400

    cleaning animation and implementing navbar features in the route

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   results: true,
+  query: null,
   queryParams: {
     q: {refreshModel: true}
   },
@@ -24,5 +25,38 @@ export default Ember.Route.extend({
   deactivate: function(){
     Ember.$('body').removeClass('hide-scroll');
     Ember.$('html').css({"overflow-y": 'scroll'});
-  }
+  },
+  actions: {
+        create() {
+          this.transitionTo('conference.new').then(function(newRoute) {
+            newRoute.controller.set('displayErrors',false);
+          });
+        },
+        scrollit() {
+          let shift = this.controllerFor('index');
+          shift.set('visited',true);
+          Ember.$('#indexTop').hide(2000);
+        },
+        tileView() {
+          Ember.$('#tileButton').addClass('disabled');
+          Ember.$('#listButton').removeClass('disabled');
+          let shift = this.controllerFor('index');
+          shift.set('tileview', true );
+        },
+        listView() {
+          Ember.$('#listButton').addClass('disabled');
+          Ember.$('#tileButton').removeClass('disabled');
+          let shift = this.controllerFor('index');
+          shift.set('tileview', false );
+        },
+        filter(params) {
+          let query = params;
+          this.transitionTo('index', {queryParams: {q: query}});
+        },
+        search(params)
+        {
+          let query = params;
+          this.transitionTo('search', {queryParams: {q: query, p:1}});
+        }
+      }
 });
