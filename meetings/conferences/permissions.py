8d9b377f44708df42f6e4484594b5ad3c84e0beb
Jesse Nayak <jdn4ae@virginia.edu>
2016-07-15 10:42:22 -0400

    Add permissions classes defined in google docs

--- meetings/conferences/permissions.py
+++ meetings/conferences/permissions.py
@@ -5,11 +5,11 @@ class CustomObjectPermissions(permissions.DjangoObjectPermissions):
     Similar to `DjangoObjectPermissions`, but adding 'view' permissions.
     """
     perms_map = {
-        'GET': ['%(app_label)s.view_%(model_name)s'],
-        'OPTIONS': ['%(app_label)s.view_%(model_name)s'],
-        'HEAD': ['%(app_label)s.view_%(model_name)s'],
-        'POST': ['%(app_label)s.add_%(model_name)s'],
-        'PUT': ['%(app_label)s.change_%(model_name)s'],
-        'PATCH': ['%(app_label)s.change_%(model_name)s'],
-        'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+        'GET': ['%(app_label)s.public'],
+        'OPTIONS': ['%(app_label)s.public'],
+        'HEAD': ['%(app_label)s.public'],
+        'POST': ['%(app_label)s.current_osf_user'],
+        'PUT': ['%(app_label)s.conference_admin', '%(app_label)s.osf_admin'],
+        'PATCH': ['%(app_label)s.conference_admin', '%(app_label)s.osf_admin'],
+        'DELETE': ['%(app_label)s.conference_admin', '%(app_label)s.osf_admin'],
     }
