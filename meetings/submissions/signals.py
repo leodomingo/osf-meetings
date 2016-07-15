1848c7cfd63545540b125dc758d3e1b251918177
Jesse Nayak <jdn4ae@virginia.edu>
2016-07-15 16:09:01 -0400

    Fix signals submissions

--- meetings/submissions/signals.py
+++ meetings/submissions/signals.py
@@ -8,12 +8,12 @@ from guardian.shortcuts import assign_perm
 
 @receiver(post_save, sender=Submission)
 def add_permissions(sender, **kwargs):
-    conference, created = kwargs["instance"], kwargs["created"]
+    submission, created = kwargs["instance"], kwargs["created"]
     if created:
         # submission_contributor: get change, delete, view permissions
-        assign_perm("submissions.change_submission", conference.admin, conference)
-        assign_perm("submissions.delete_submission", conference.admin, conference)
-        assign_perm("submissions.view_submission", conference.admin, conference)
+        assign_perm("submissions.change_submission", submission.contributor, conference)
+        assign_perm("submissions.delete_submission", submission.contributor, conference)
+        assign_perm("submissions.view_submission", submission.contributor, conference)
 
         # current_osf_user: doesn't get any permissions initially
         current_osf_users = Group.objects.get(name=OsfOauth2AdapterConfig.osf_users_group)
