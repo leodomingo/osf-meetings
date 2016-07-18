befb5ea8167f39f8b338162af4917571f03d82d3
Bryan Gorges <bgorges@cos.io>
2016-07-18 15:04:30 -0400

    Change a few things

--- meetings/meetings/settings/local-dist.py
+++ meetings/meetings/settings/local-dist.py
@@ -1,5 +1,15 @@
 """Copy this file into local.py for local development"""
 
+from defaults import BASE_DIR
+import os
+
 
 SECRET_KEY = ''  # Set to large random value in your local.py
 DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
+    }
+}
--- meetings/meetings/settings/local.py
+++
@@ -1,5 +0,0 @@
-"""Copy this file into local.py for local development"""
-
-
-SECRET_KEY = '^j!ubi^$@*kwkt$omnpgt+o!6mgysh7kk_-e+0%!8k=lt(md61'
-DEBUG = True
