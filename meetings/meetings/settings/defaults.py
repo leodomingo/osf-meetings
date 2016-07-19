85b79cf92047bd0999a9f30cf8043f246bfc0613
Bryan Gorges <bgorges@cos.io>
2016-07-19 08:37:09 -0400

    Add postgres settings to distrib

--- meetings/meetings/settings/defaults.py
+++ meetings/meetings/settings/defaults.py
@@ -108,9 +108,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'meetings.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
--- meetings/meetings/settings/local-dist.py
+++ meetings/meetings/settings/local-dist.py
@@ -1,15 +1,21 @@
 """Copy this file into local.py for local development"""
 
-from defaults import BASE_DIR
-import os
-
 
 SECRET_KEY = ''  # Set to large random value in your local.py
 DEBUG = True
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
-    }
-}
+# Database
+# POSTGRES_NAME = ''
+# POSTGRES_USER = ''
+# POSTGRES_PASSWORD = ''
+# POSTGRES_HOST = ''
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': POSTGRES_NAME,
+#         'USER': POSTGRES_USER,
+#         'PASSWORD': POSTGRES_PASSWORD,
+#         'HOST': POSTGRES_HOST,
+#         'PORT': '',
+#     }
+# }
