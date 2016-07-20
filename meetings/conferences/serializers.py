e8afaa8ce370b52b5949e25b8c87299188adb69b
Bryan Gorges <bgorges@cos.io>
2016-07-20 12:34:44 -0400

    Fix flake8

--- meetings/conferences/serializers.py
+++ meetings/conferences/serializers.py
@@ -5,7 +5,6 @@ from conferences.models import Conference
 from submissions.models import Submission
 
 
-
 class ConferenceSerializer(serializers.ModelSerializer):
     links = serializers.SerializerMethodField()
     submission_count = serializers.SerializerMethodField()
