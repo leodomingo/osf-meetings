7d9738db35e8da2df88d9645ec0baa3a1ce5330d
Tav0 <lv23@vt.edu>
2016-07-20 09:17:28 -0400

    add dropzone widget

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,22 +1,9 @@
 import DS from 'ember-data';
+import OsfAdapter from './osf-adapter';
 import Ember from 'ember';
 
-export default DS.JSONAPIAdapter.extend({
-    host: 'http://localhost:8000',
-    buildURL(modelName, id, snapshot, requestType) {
-        // Fix issue where CORS request failed on 301s: Ember does not seem to append trailing
-        // slash to URLs for single documents, but DRF redirects to force a trailing slash
-        var url = this._super(...arguments);
-        if (requestType === 'deleteRecord' || requestType === 'updateRecord' || requestType === 'findRecord') {
-            if (snapshot.record.get('links.self')) {
-                url = snapshot.record.get('links.self');
-            }
-        }
-        if (url.lastIndexOf('/') !== url.length - 1) {
-            url += '/';
-        }
-        return url;
-    },
+export default OsfAdapter.extend({
+    namespace : '',
     ajax: function(url, method, hash) {
         hash.crossDomain = true;
         hash.xhrFields = {withCredentials: true};
