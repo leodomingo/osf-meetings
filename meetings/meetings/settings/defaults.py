3d359720418c33d3a97d905bfb3fb5fb4960a297
Leandro Alberto-Dominguez <la5aj@virginia.edu>
2016-07-26 10:49:57 -0400

    adding elasticsearch config to settings

--- meetings/meetings/settings/defaults.py
+++ meetings/meetings/settings/defaults.py
@@ -154,6 +154,15 @@ CORS_ORIGIN_WHITELIST = (
     'localhost:4200',
 )
 
+#Haystack settings 
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://127.0.0.1:9200/',
+        'INDEX_NAME': 'haystack',
+    },
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
