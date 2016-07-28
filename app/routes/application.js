5a4f99c435a92fab69971531e51c4abce0a0f55e
Jesse Nayak <jdn4ae@virginia.edu>
2016-07-28 14:26:38 -0400

    Add user model

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export
 default Ember.Route.extend({
+    model(){
+        return this.store.findRecord('user', 'me');
+    },
     actions: {
         logout: function() {
             this.transitionTo('logout');
