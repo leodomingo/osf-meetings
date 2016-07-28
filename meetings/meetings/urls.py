85653333415b7c12c80e067e640261196a958885
Jesse Nayak <jdn4ae@virginia.edu>
2016-07-28 13:51:46 -0400

    Add token and change current endpoint to users/me

--- meetings/meetings/urls.py
+++ meetings/meetings/urls.py
@@ -4,20 +4,19 @@ from django.contrib import admin
 from rest_framework import routers
 
 # Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 
-router = routers.DefaultRouter()
-router.register(r'users', apiViews.UserViewSet)
+# Additionally, we include login URLs for the browsable API.
 
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^conferences/', include('conferences.urls', namespace='conferences')),
-    url(r'^submissions/', include('submissions.urls', namespace='submissions')),
-    url(r'^checklogin/', apiViews.CheckLoggedInView.as_view(), name='checklogin'),
-    url(r'^current/', apiViews.CurrentUserView.as_view(), name='current'),
+    url(r'^conferences/',
+        include('conferences.urls', namespace='conferences')),
+    url(r'^submissions/',
+        include('submissions.urls', namespace='submissions')),
+    url(r'^checklogin/', apiViews.CheckLoggedInView.as_view(),
+        name='checklogin'),
+    url(r'^users/me', apiViews.CurrentUserView.as_view(), name='current'),
     url(r'^admin/', admin.site.urls, name='admin'),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^mail/inbound/', include('mail.urls')),
-    url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
 ]
