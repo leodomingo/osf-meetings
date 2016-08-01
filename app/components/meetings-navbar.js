188de1f1d7579f506f5b5f9957c44fa5e7a8951f
Leandro Alberto-Dominguez <la5aj@virginia.edu>
2016-08-01 11:43:47 -0400

    documenting navbar and search workflow

--- app/components/meetings-navbar.js
+++ app/components/meetings-navbar.js
@@ -3,27 +3,24 @@ import Ember from 'ember';
 import config from '../config/environment';
 
 export default Ember.Component.extend({
-    fixed: true,
-    routing: Ember.inject.service('-routing'),
-    host: config.osfUrl,
-    authenticated: false,
-    frontPage: null,
-    user: null,
-    onSearchPage: false,
-    institution: true,
-    showSearch: false,
+    fixed: true, /* holds status of navbar -> fixed or unfixed <- depending on mobile view or not */
+    host: config.osfUrl, /* root URL for Osf redirection */
+    authenticated: false, /* authentication status */
+    user: null, /* current user metadata, initially empty */
+    showSearch: false, /* holds current view status of search dropdown */
+
+        /*
+        *   init: function()
+        *   Initialization sequence for the navbar 
+        *   Ajax request to the user endpoint to establish authentication
+        *   If authenticated, user metadata is populated, 
+        *   if not, then authentication status is set to false
+        *
+        */
+
     init: function() {
         this._super(...arguments);
         var self = this;
-        var currentRoute = this.get('routing').get('currentRouteName');
-
-        if (currentRoute === 'index'){
-            self.set('frontPage', true);
-        }
-        else {
-            self.set('frontPage', false);
-        }
-
         Ember.$.ajax({
             url: config.currentUser,
             dataType: 'json',
@@ -43,18 +40,33 @@ export default Ember.Component.extend({
         });
     },
     actions: {
-        filter: function() {
-            this.sendAction('filter', this.get("searchQuery"));},
-
+        /*
+        *   search()
+        *   search function sends action to route component is nested in,
+        *   carries searchQuery parameter with it  
+        */
         search: function() {
             this.sendAction('search', this.get("searchQuery"));},
 
+        /*
+        *   logout()
+        *   sends action to the application level,
+        *   subsequently logs out and removes user metadata
+        */
         logout: function() {
             this.sendAction('logout');},
 
+        /*
+        *   logout()
+        *   sends action to the application level,
+        *   subsequently logs in and adds user metadata
+        */
         login: function() {
             this.sendAction('login');},
-
+        /*
+        * unFix()
+        * Toggles position status of navbar depending on mobile or desktop view
+        */
         unFix: function() {
             if (this.get("fixed") === true){
                  Ember.$('#create').removeClass("navbar-fixed-top");
@@ -63,6 +75,9 @@ export default Ember.Component.extend({
                 Ember.$('#create').addClass("navbar-fixed-top");
                 this.set('fixed', true);}
             },
+            /*
+            * Toggles view of the dropdown search bar based on search icon click
+            */
         toggleSearch: function(){
             if (this.get("showSearch") === false){
                 this.set("showSearch", true);}
--- app/controllers/search.js
+++ app/controllers/search.js
@@ -1,20 +1,37 @@
 import Ember from 'ember';
 
+/*
+* many Controller variables appear to never be accessed here
+* they are setup in the search route after search is complete and the model is available in the store 
+*/
+
 export default Ember.Controller.extend({
-    query: null,
-    underFive: false,
-    onePage: false,
-    buttonArray: [],
-    results: null,
-    store: Ember.inject.service('store'),
-    queryParams: ['q', 'p'],
-    page: 1,
+    query: null, /* Holds the current search query */
+    underFive: false, /* For pagination UI : true if there are fewer than 5 pages of results */
+    onePage: false, /* For pagination UI : true if there is only one page of results */
+    buttonArray: [], /* 
+                      * For pagination UI : holds the current page links available
+                      * 2 before and 2 after the current page 
+                      */
+    results: null,    /* holds boolean value of there are any results at all*/
+    queryParams: ['q', 'p'], /* query params for search route, hold search query and page number */
+    page: 1, /* For pagination UI : holds current page number */
     actions: {
+        /*
+        *   search()
+        *   recieves the searchQuery sent up from the meetings navbar
+        *   takes the sent value and sets the query parameter 
+        *   transitions to search route with query parameter, setting the model with search results
+        */
         search(params) {
             let query = params;
             this.set('query', query);
             this.transitionToRoute('search', {queryParams: {q: query, p: this.page }});
         },
+        /*
+        *   toPage()
+        *   redirects to specific page number in search results
+        */
         toPage(pageNum){
             this.transitionToRoute('search', {queryParams: {p: pageNum }});
         }
