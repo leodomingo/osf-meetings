7145272dfa110ec723865834047101e264d6d277
Tav0 <lv23@vt.edu>
2016-08-02 11:33:04 -0400

    minor fix

--- app/helpers/date-display.js
+++ app/helpers/date-display.js
@@ -4,7 +4,7 @@ import moment from 'moment';
 
 export function dateDisplay(params/*, hash*/) {
     var date = new Date(params[0]);
-    return moment(date).format("MM-DD-YYYY")
+    return moment(date).format("MM-DD-YYYY");
 }
 
 export default Ember.Helper.helper(dateDisplay);
