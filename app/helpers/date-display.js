f2c3f2cc973144bdd7900ae304e3113e8abe5bdc
Tav0 <lv23@vt.edu>
2016-08-02 03:15:34 -0400

    connecting missing part between client and backend and fix some bugs

--- app/helpers/date-display.js
+++ app/helpers/date-display.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
+import moment from 'moment';
 
 
 export function dateDisplay(params/*, hash*/) {
-	var date = new Date(params[0]);
-	return date.toString();
+    var date = new Date(params[0]);
+    return moment(date).format("MM-DD-YYYY")
 }
 
 export default Ember.Helper.helper(dateDisplay);
