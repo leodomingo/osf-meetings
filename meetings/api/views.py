2085e838a0f05b1a7a7340f59997bca986f78686
Leandro Alberto-Dominguez <la5aj@virginia.edu>
2016-08-09 13:40:20 -0400

    doing it for Travis

--- meetings/api/views.py
+++ meetings/api/views.py
@@ -7,7 +7,7 @@ from rest_framework import status
 import requests
 import json
 from rest_framework.decorators import api_view, renderer_classes
-from rest_framework import response, schemas
+from rest_framework import response
 from rest_framework_swagger.renderers import OpenAPIRenderer, SwaggerUIRenderer
 
 
