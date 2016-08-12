8f4414a892fec229a8a2eab9b302c87aae28a7c5
Tav0 <lv23@vt.edu>
2016-08-12 14:02:26 -0400

    validations in submission model

--- app/models/submission.js
+++ app/models/submission.js
@@ -1,8 +1,37 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { belongsTo } from 'ember-data/relationships';
+import { validator, buildValidations } from 'ember-cp-validations';
 
-export default Model.extend({
+var Validations = buildValidations({
+    title: {
+        description: 'Title',
+        validators: [
+            validator('presence', true),
+            validator('length', {
+                min: 4
+            }),
+            validator('format', {
+                regex: /^(?=.*[a-z])(?=.*[A-Z]).{4,8}$/,
+                message: '{title} must have the first letter capitalized'
+            })
+        ]
+    },
+    description: {
+        description: 'Description',
+        validators: [
+            validator('presence', true),
+            validator('length', {
+                min: 10
+            })
+        ]
+    },
+}, {
+    debounce: 500
+});
+
+
+export default Model.extend(Validations, {
     conference : belongsTo('conference'),
     title : attr('string'),
     description : attr('string'),
