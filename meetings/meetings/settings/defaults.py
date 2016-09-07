ff0d931ecd713ddcfdca394f8f1703c233ea3667
Leandro Alberto-Dominguez <la5aj@virginia.edu>
2016-09-07 15:47:25 -0400

    extending logout template

--- meetings/meetings/settings/defaults.py
+++ meetings/meetings/settings/defaults.py
@@ -218,7 +218,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/' 
+STATIC_URL = '/static/'
 SITE_ID = 1
 
 OSF_API_URL = ('https://staging-api.osf.io').rstrip('/') + '/'
